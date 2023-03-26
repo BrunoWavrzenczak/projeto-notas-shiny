ede7b0a6d6e0a5ed10246fbbe3a1173c374e8103
Wavrzenczak <brunowavrzenczak@gmail.com>
2023-03-26 10:23:19 -0300

    atualiza shiny notas

--- server.R
+++ server.R
@@ -26,17 +26,17 @@ shinyServer(function(input, output, session) {
         req(data())
 
         validate(
-            need(nrow(data()) == 0, "Não temos informação para esse periodo")
+            need(nrow(dplyr::filter(data(),prova == 1)) != 0, "Sem informação")
         )
 
         data() |>
-            dplyr::filter(prova == "final") |> 
+            dplyr::filter(prova == "1") |> 
             ggplot(aes(x = notas)) +
             geom_histogram(
                 color = 'black', binwidth = 1, boundary = 0,
                 fill = c(rep('coral1', 4), rep('3', 3), rep('4', 3))
             ) +
-            labs(title = paste('média final de', input$materia), x = 'notas', y = 'frequencia') +
+            labs(title = paste('p1', input$materia), x = 'notas', y = 'frequencia') +
             scale_x_continuous(limits = c(0, 10), breaks = seq(0, 10, 1)) +
             scale_y_continuous(limits = c(0, 20), breaks = seq(0, 20, 2)) +
             geom_vline(aes(xintercept = mean(notas), color = 'média'), size = 1.5) +
@@ -49,7 +49,7 @@ shinyServer(function(input, output, session) {
         req(data())
 
         validate(
-            need(nrow(data()) == 0, "Não temos informação para esse periodo")
+            need(nrow(dplyr::filter(data(),prova == 2)) != 0, "Sem informação")
         )
 
         data() |>
@@ -65,53 +65,47 @@ shinyServer(function(input, output, session) {
             theme_minimal()
     })
 
-    # # ==============================================================================
-    # output$p3 <- renderPlot({
-    #     switch(input$periodo,
-    #         '1' = NULL,
+    # ==============================================================================
+    output$p3 <- renderPlot({
+      req(data())
 
-    #         '2' = ggplot(dplyr::filter(tb, periodo == input$periodo,
-    #             materia == input$materia,
-    #             prova == 3), aes(x = notas)) +
-    #             geom_histogram(color = 'black', binwidth = 1, boundary = 0,
-    #                 fill = c(rep('coral1', 4), rep('3', 3), rep('4', 3))) +
-    #             labs(title = paste('p3', input$materia), x = 'notas', y = 'frequencia') +
-    #             scale_x_continuous(limits = c(0, 10), breaks = seq(0, 10, 1)) +
-    #             scale_y_continuous(limits = c(0, 20), breaks = seq(0, 20, 2)) +
-    #             geom_vline(aes(xintercept = mean(notas), color = 'média'), size = 1.5) +
-    #             scale_color_manual('', values = 'black') +
-    #             theme_minimal())
-    # })
-    # # ==============================================================================
+      validate(
+        need(nrow(dplyr::filter(data(),prova == 3)) != 0, 'Sem informação')
+      )
 
-    # output$bar <- renderPlot({
-    #     switch(input$periodo,
+      data() |>
+        dplyr::filter(prova == '3') |>
+        ggplot(aes(x = notas)) +
+        geom_histogram(color = 'black', binwidth = 1, boundary = 0,
+                       fill = c(rep('coral1', 4), rep('3', 3), rep('4', 3))) +
+        labs(title = paste('p3', input$materia), x = 'notas', y = 'frequencia') +
+        scale_x_continuous(limits = c(0, 10), breaks = seq(0, 10, 1)) +
+        scale_y_continuous(limits = c(0, 20), breaks = seq(0, 20, 2)) +
+        geom_vline(aes(xintercept = mean(notas), color = 'média'), size = 1.5) +
+        scale_color_manual('', values = 'black') +
+        theme_minimal()
+     })
+     # ==============================================================================
 
-    #         '1' = ggplot(
-    #             dplyr::filter(tb, periodo == input$periodo,
-    #                 materia == input$materia, prova == 'final'),
-    #             aes(x = factor(intervalo, levels = c('[0,4]', '(4,7]', '(7,10]')),
-    #                 fill = intervalo)) +
-    #             geom_bar() +
-    #             scale_fill_manual('', values = c('coral1', '3', '4'),
-    #                 breaks = c('[0,4]', '(4,7]', '(7,10]'),
-    #                 labels = c('reprovaram', 'final', 'passaram')) +
-    #             labs(x = '', y = '') +
-    #             scale_y_continuous(limits = c(0, 20), breaks = seq(0, 20, 2)),
+    output$bar <- renderPlot({
+      req(data())
+      
+      validate(
+        need(nrow(dplyr::filter(data(),prova == 'final')) != 0, 'Sem informação')
+      )
 
-    #         '2' = ggplot(
-    #             dplyr::filter(tb, periodo == input$periodo,
-    #                 materia == input$materia, prova == 'final'),
-    #             aes(x = factor(intervalo, levels = c('[0,4]', '(4,7]', '(7,10]')),
-    #                 fill = intervalo)) +
-    #             geom_bar() +
-    #             scale_fill_manual('', values = c('coral1', '3', '4'),
-    #                 breaks = c('[0,4]', '(4,7]', '(7,10]'),
-    #                 labels = c('reprovaram', 'final', 'passaram')) +
-    #             labs(x = '', y = '') +
-    #             scale_y_continuous(limits = c(0, 50), breaks = seq(0, 50, 5))
+      data() |>
+        dplyr::filter(prova == 'final') |>
+        ggplot(
+          aes(x = factor(intervalo, levels = c('[0,4]', '(4,7]', '(7,10]')),
+              fill = intervalo)) +
+        geom_bar() +
+        scale_fill_manual('', values = c('coral1', '3', '4'),
+                          breaks = c('[0,4]', '(4,7]', '(7,10]'),
+                          labels = c('reprovaram', 'final', 'passaram')) +
+        labs(x = '', y = '') +
+        scale_y_continuous(limits = c(0, 50), breaks = seq(0, 50, 5))+
+        theme_minimal()
 
-    #     )
-
-    # })
+     })
 })
--- ui.R
+++ ui.R
@@ -1,5 +1,5 @@
 dashboardPage(
-    skin = 'green',
+    skin = 'blue',
     dashboardHeader(title = 'Notas GRR2022'),
     dashboardSidebar(
         sidebarMenu(
@@ -11,16 +11,16 @@ dashboardPage(
         tabItems(
             tabItem('provas', fluidPage(
                 fluidRow(
-                    column(6, box(selectInput('periodo', 'período', choices = c('1', '2'), '1'), width = 12)),
-                    column(6, box(selectInput('materia', 'matéria', c('est.básica', 'matriz', 'função'), 'est.básica'), width = 12))),
+                    column(6, box(selectInput('periodo', 'período', choices = c('1', '2'), '1'), width = 12,background = 'navy')),
+                    column(6, box(selectInput('materia', 'matéria', c('est.básica', 'matriz', 'função'), 'est.básica'), width = 12,background = 'navy'))),
 
                 fluidRow(
-                    column(6, box(plotOutput('p1'), width = 12)),
-                    column(6, box(plotOutput('p2'), width = 12))),
+                    column(6, box(plotOutput('p1'), width = 12, title = 'Prova-1',background = 'blue')),
+                    column(6, box(plotOutput('p2'), width = 12, title = 'Prova-2',background = 'blue'))),
 
                 fluidRow(
-                    column(6, box(plotOutput('p3'), width = 12)),
-                    column(6, box(plotOutput('bar'), width = 12)))
+                    column(6, box(plotOutput('p3'), width = 12, title = 'Prova-3',background = 'blue')),
+                    column(6, box(plotOutput('bar'), width = 12, title = 'Resultado Final',background = 'purple')))
             ))
 
         )
